2154f370c48a321896eeaead0253de24659c0541
Satyam Kumar Choudhary <choudharysatyam951478@gmail.com>
2024-06-16 14:47:52 +0530

    Initial

---
+++ ConstObject.js
@@ -0,0 +1,6 @@
+const item = {
+    name:"Raj",
+    age: 95,
+    gender: "male"
+};
+console.log(item.name,item.age,item.gender);
---
+++ addingNumberAndString.js
@@ -0,0 +1,4 @@
+let a="Hello";
+a=a+256666;
+console.log(a);
+console.log(typeof(a));
